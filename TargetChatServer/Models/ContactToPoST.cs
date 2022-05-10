2b5af1fc45db66dfb9ebc5f76bdd9dcfc3bfa734
orel <orelbenshamai@gmail.com>
2022-05-10 20:35:08 +0300

    deqewq

---
+++ TargetChatServer/Models/ContactToPoST.cs
@@ -0,0 +1,11 @@
+﻿namespace targetchatserver.Models
+{
+    public class ContactToPost
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string server { get; set; }
+        public string? last { get; set; }
+        public DateTime? lastdate { get; set; }
+    }
+}
