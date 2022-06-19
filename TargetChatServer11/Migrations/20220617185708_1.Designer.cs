cc4fcbfa2912d8d341193512c63ece6fcc0fb1d6
Etelis <github@redirecto.me>
2022-06-19 22:54:38 +0300

    Update to user model
    Update to user model

--- TargetChatServer11/Migrations/20220617185708_1.Designer.cs
+++ TargetChatServer11/Migrations/20220617185708_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TargetChatServer11.Data;
 
@@ -11,9 +12,10 @@ using TargetChatServer11.Data;
 namespace TargetChatServer11.Migrations
 {
     [DbContext(typeof(TargetChatServer11Context))]
-    partial class TargetChatServer11ContextModelSnapshot : ModelSnapshot
+    [Migration("20220617185708_1")]
+    partial class _1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TargetChatServer11/Models/UserModel.cs
+++ TargetChatServer11/Models/UserModel.cs
@@ -6,15 +6,18 @@ namespace TargetChatServer11.Models
 {
     public class UserModel
     {
+        [IgnoreDataMember]
         [Key]
         public int Id { get; set; }
         [Required]
         public string Username { get; set; }
         [Required]
+        [IgnoreDataMember]
         public string Password { get; set; }
         [Required]
         public string DisplayName { get; set; }
         public string? Photo { get; set; }
+        [IgnoreDataMember]
         public List<Contact>? Contacts { get; set; }
         [IgnoreDataMember]
         public List<AndroidDeviceIDModel>? DeviceIds { get; set; }
